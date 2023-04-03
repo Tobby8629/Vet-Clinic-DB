98940cc6204dad465bdbfe02a3acb5bb51cba919
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-03 14:31:59 +0300

    Revert of parts completed by Alwyn

--- schema.sql
+++ schema.sql
@@ -52,22 +52,8 @@ CREATE TABLE specialization (
   vets_id BIGINT REFERENCES vets (id)
 );
 
---create a many to many relationship between vets and animals 
-CREATE TABLE visits (
-  id BIGSERIAL PRIMARY KEY,
-  animals_id BIGINT REFERENCES animals (id),
-  vets_id BIGINT REFERENCES vets (id),
-  visit_date DATE
-);
-
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
-
---create index for animals_id on visits table
-CREATE INDEX animal_id_asc ON visits(animals_id ASC);
-
---create index for vets_id on visits table
-CREATE INDEX vet_id_asc ON visits(vets_id ASC);
 
 --create index for email on owners table
 CREATE INDEX email_asc ON owners( email ASC);
